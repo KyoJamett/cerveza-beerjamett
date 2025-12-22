ee5e40340a74465e4ccff6ff91f958a17bffcc0e
Ignacio <kyo.akamatsu@gmail.com>
2025-12-22 00:09:23 -0300

    feat: map added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -35,7 +35,15 @@ export const Contact = () => {
             <div className="col-md-6  px-0">
               <div className="map_container">
                 <div className="map">
-                  <div id="googleMap"></div>
+                  <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d468.12214979754725!2d-71.1756092522793!3d-32.48377284584005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x968853f2247ab17f%3A0x1074edde18e0d5c9!2sBeerJamett!5e1!3m2!1ses!2scl!4v1766372614773!5m2!1ses!2scl"
+                    width="600"
+                    height="450"
+                    style={{ border: 0 }}
+                    allowfullscreen=""
+                    loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"
+                  ></iframe>
                 </div>
               </div>
             </div>
