cc145f73ed55a64d95a9f1f976a77f2cd0006d6b
Ignacio <kyo.akamatsu@gmail.com>
2026-02-13 12:50:03 -0300

    reactor: testimonial section replaced for gallery section

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,101 +1,105 @@
-import Slider from "react-slick";
-
-const clients = [
-  {
-    name: "Gero Miliya",
-    desc: "long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it haslong established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it haslong established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has",
-    img: "images/client-img.jpg",
-  },
-  {
-    name: "Gero Miliya",
-    desc: "long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it haslong established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it haslong established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has",
-    img: "images/client-img.jpg",
-  },
-  {
-    name: "Gero Miliya",
-    desc: "long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it haslong established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it haslong established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has",
-    img: "images/client-img.jpg",
-  },
-];
-
-const ClientCard = ({ client }) => {
-  return (
-    <div className="box">
-      <div className="img-box">
-        <img src={client.img} alt={client.name} className="img-fluid" />
-      </div>
-      <div className="detail-box">
-        <h4>{client.name}</h4>
-        <p>{client.desc}</p>
-        <i className="fa fa-quote-left" aria-hidden="true"></i>
-      </div>
-    </div>
-  );
-};
-
-const CustomPrevArrow = (props) => {
-  const { onClick } = props;
-  return (
-    <button className="carousel-control-prev" onClick={onClick}>
-      <i className="fa fa-arrow-left" aria-hidden="true"></i>
-      <span className="visually-hidden">Previous</span>
-    </button>
-  );
-};
-
-const CustomNextArrow = (props) => {
-  const { onClick } = props;
-  return (
-    <button className="carousel-control-next" onClick={onClick}>
-      <i className="fa fa-arrow-right" aria-hidden="true"></i>
-      <span className="visually-hidden">Next</span>
-    </button>
-  );
-};
-
 export const Clients = () => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 5000,
-    pauseOnHover: true,
-    arrows: true,
-    prevArrow: <CustomPrevArrow />,
-    nextArrow: <CustomNextArrow />,
-  };
   return (
-    <>
-      <section id="clients" className="client_section">
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-lg-4 ms-auto">
-              <div className="img-box sub_img-box">
+    <div id="about" className="container-xxl py-5">
+      <div className="container">
+        <div className="row g-5">
+          <div className="col-lg-6">
+            <div className="row g-3">
+              <div className="col-6 text-end">
                 <img
-                  src="images/client-chocolate.png"
+                  className="img-fluid bg-white w-100 mb-3"
+                  data-aos="fade"
+                  data-aos-delay="100"
+                  src="images/gallery-01.jpg"
                   alt=""
-                  className="img-fluid"
+                />
+                <img
+                  className="img-fluid bg-white w-50"
+                  data-aos="fade"
+                  data-aos-delay="200"
+                  src="images/gallery-02.jpg"
+                  alt=""
+                />
+                <img
+                  className="img-fluid bg-white w-100 mt-3"
+                  data-aos="fade"
+                  data-aos-delay="400"
+                  src="images/gallery-04.jpg"
+                  alt=""
+                />
+              </div>
+              <div className="col-6">
+                <img
+                  className="img-fluid bg-white w-50 mb-3"
+                  data-aos="fade"
+                  data-aos-delay="300"
+                  src="images/gallery-03.jpeg"
+                  alt=""
+                />
+                <img
+                  className="img-fluid bg-white w-100"
+                  data-aos="fade"
+                  data-aos-delay="400"
+                  src="images/gallery-04.jpg"
+                  alt=""
+                />
+                <img
+                  className="img-fluid bg-white w-100 mb-3 mt-3"
+                  data-aos="fade"
+                  data-aos-delay="100"
+                  src="images/gallery-01.jpg"
+                  alt=""
                 />
               </div>
             </div>
-            <div className="col-lg-6 px-0">
-              <div className="client_container">
-                <div className="heading_container">
-                  <h2>Ellos ya la probaron</h2>
-                </div>
-                <Slider {...settings}>
-                  {clients.map((client) => {
-                    return <ClientCard key={client.key} client={client} />;
-                  })}
-                </Slider>
+          </div>
+          <div className="col-lg-6" data-aos="fade" data-aos-delay="500">
+            <div className="section-title">
+              <p className="fs-5 fw-medium fst-italic text-primary">
+                Nuestro sello
+              </p>
+              <h1 className="display-6">
+                Más que una cervecería, somos una comunidad de amantes de la
+                buena cerveza.
+              </h1>
+            </div>
+            <div className="row g-3 mb-4">
+              <div className="col-sm-4">
+                <img
+                  className="img-fluid bg-white w-100"
+                  src="images/gallery-08.jpg"
+                  alt=""
+                />
+              </div>
+              <div className="col-sm-8">
+                <h5>Producto 100% artesanal</h5>
+                <p className="mb-0">
+                  Cada cerveza que producimos es el resultado de incontables
+                  horas de experimentación, pasión y el deseo de ofrecer algo
+                  verdaderamente especial a nuestra comunidad.
+                </p>
+              </div>
+            </div>
+            <div className="border-top mb-4"></div>
+            <div className="row g-3">
+              <div className="col-sm-8">
+                <h5>Calidad premium</h5>
+                <p className="mb-0">
+                  Creemos en el poder de reunir a las personas alrededor de una
+                  buena cerveza y en crear momentos memorables.
+                </p>
+              </div>
+              <div className="col-sm-4">
+                <img
+                  className="img-fluid bg-white w-100"
+                  src="images/gallery-07.jpg"
+                  alt=""
+                />
               </div>
             </div>
           </div>
         </div>
-      </section>
-    </>
+      </div>
+    </div>
   );
 };
